1d5b9b57d5b829bd3795e1d70ead8ca156b244cc
nayarasilva <nayarasilva@unasus.gov.br>
2020-11-24 22:22:33 -0300

    Codigo com comentarios sobre o que precisamos modificar para terminar o trabalho

--- main.go
+++ main.go
@@ -1,6 +1,29 @@
 package main
 
+import (
+	"log"
+	"net"
+)
 
 func main() {
-   
+	s := newServer()//Cria o server
+	go s.run()//manda o server rodar as funções da lista de comandos
+//Faz o server esperar pelo Client
+	listener, err := net.Listen("tcp", ":8888")
+	if err != nil {
+		log.Fatalf("unable to start server: %s", err.Error())
+	}
+	defer listener.Close()
+	log.Printf("server started on :8888")
+	
+	for {
+		//For para sempre que fica esperando o Cliente conectar
+		conn, err := listener.Accept()//Cliente connectando
+		if err != nil {
+			log.Printf("failed to accept connection: %s", err.Error())
+			continue
+		}
+
+		go s.newClient(conn)
+	}
 }
---
+++ server.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+type server struct {
+	commands chan 
+	members map[net.Addr]*client
+}
+
+func newServer() *server {
+	return &server{
+		// rooms:    make(map[string]*room),
+		commands: make(chan command),
+		members : make(map[net.Addr]*client),
+	}
+}
+
+func (s *server) run() {
+	for cmd := range s.commands {
+		switch cmd.id {
+		case CMD_NICK:
+			s.nick(cmd.client, cmd.args[1])
+		// case CMD_JOIN:
+		// 	s.join(cmd.client, cmd.args[1])
+		// case CMD_ROOMS:
+		// 	s.listRooms(cmd.client)
+		case CMD_MSG:
+			s.msg(cmd.client, cmd.args)
+		case CMD_QUIT://Modificar
+			s.quit(cmd.client)
+		}
+	}
+}
+
+func (s *server) newClient(conn net.Conn) {
+	log.Printf("new client has joined: %s", conn.RemoteAddr().String())
+	//Criar funçao "qual o seu nick" e setar variavel temporario para input do usuario
+	c := &client{
+		conn:     conn,
+		nick:     "anonymous",//variavel temporario
+		commands: s.commands,
+	}
+
+	c.readInput()
+}
+//Essa funçao nick ja cria o nick
+func (s *server) nick(c *client, nick string) {
+	//adicionar printf: qual o seu nick
+	c.nick = nick
+	c.msg(fmt.Sprintf("all right, I will call you %s", nick))
+}
+
+// func (s *server) join(c *client, roomName string) {
+// 	r, ok := s.rooms[roomName]
+// 	if !ok {
+// 		r = &room{
+// 			name:    roomName,
+// 			members: make(map[net.Addr]*client),
+// 		}
+// 		s.rooms[roomName] = r
+// 	}
+// 	r.members[c.conn.RemoteAddr()] = c
+
+// 	s.quitCurrentRoom(c)
+// 	c.room = r
+
+// 	r.broadcast(c, fmt.Sprintf("%s joined the room", c.nick))
+
+// 	c.msg(fmt.Sprintf("welcome to %s", roomName))
+// }
+
+// func (s *server) listRooms(c *client) {
+// 	var rooms []string
+// 	for name := range s.rooms {
+// 		rooms = append(rooms, name)
+// 	}
+
+// 	c.msg(fmt.Sprintf("available rooms: %s", strings.Join(rooms, ", ")))
+// }
+
+func (s *server) msg(c *client, args []string) {
+	msg := strings.Join(args[1:len(args)], " ")
+	c.room.broadcast(c, c.nick+": "+msg)
+	//s.broadcast(c, c.nick+": "+msg)
+}
+
+func (s *server) quit(c *client) {
+	log.Printf("client has left the chat: %s", c.conn.RemoteAddr().String())
+	//Sair do servidor
+	s.quitCurrentRoom(c)
+
+	c.msg("sad to see you go =(")
+	// c.conn.Close()
+	conn.Close();
+}
+
+// func (s *server) quitCurrentRoom(c *client) {
+// 	if c.room != nil {
+// 		oldRoom := s.rooms[c.room.name]
+// 		delete(s.rooms[c.room.name].members, c.conn.RemoteAddr())
+// 		oldRoom.broadcast(c, fmt.Sprintf("%s has left the room", c.nick))
+// 	}
+// }
+
