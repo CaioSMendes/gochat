b946fa24165340da683d7f3bbe31c39b63ae5a44
CaioSMendes <caioitallo.souzamendes@gmail.com>
2020-12-06 16:27:00 -0300

    mostra o ip

--- server.go
+++ server.go
@@ -42,7 +42,7 @@ func (s *server) run() {
 func (s *server) newClient(conn net.Conn) {
 	//colorGreen := "\033[32m"
 	//log.Printf("new client has joined: %s", conn.RemoteAddr().String())
-	log.Println("Cliente novo: %s", conn.RemoteAddr().String())
+	log.Printf("Cliente novo: %s", conn.RemoteAddr().String())
 	//Criar funçao "qual o seu nick" e setar variavel temporario para input do usuario
 	c := &client{
 		conn:     conn,
